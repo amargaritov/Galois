ac59a2fb0833cb5bfdf5bdaf37e358a8e90912ea
Dmitrii Ustiugov <dmitrii.ustiugov@epfl.ch>
2018-11-04 17:24:10 +0100

    added build script

---
+++ build_lonestar.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -e
+
+# GCC-6 installation
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo apt-get update
+sudo apt-get install gcc-6 g++-6 -y
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 60 --slave /usr/bin/g++ g++ /usr/bin/g++-6 && gcc -v
+
+sudo apt-get install -y cmake3 make 
+
+if [ -d ./build ]; then
+    echo "./build exists!"
+    exit
+fi
+
+mkdir ./build
+cd ./build
+cmake ..
+cd lonestar/pagerank
+make -j
+cd ../bfs
+make -j
+
+
