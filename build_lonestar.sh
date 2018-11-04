ace71b12fe260207ed7ae895f588ec37cd4d1f3a
Dmitrii Ustiugov <dmitrii.ustiugov@epfl.ch>
2018-11-04 18:35:40 +0100

    added libnuma-dev to galois setup script

--- build_lonestar.sh
+++ build_lonestar.sh
@@ -6,7 +6,7 @@ sudo apt-get update
 sudo apt-get install gcc-6 g++-6 -y
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 60 --slave /usr/bin/g++ g++ /usr/bin/g++-6 && gcc -v
 
-sudo apt-get install -y cmake3 make 
+sudo apt-get install -y cmake3 make libnuma-dev
 
 if [ -d ./build ]; then
     echo "./build exists!"
