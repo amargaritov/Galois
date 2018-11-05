2be9b6be00e0ec7ebf1c32cac63c25b467f13505
Dmitrii Ustiugov <dmitrii.ustiugov@epfl.ch>
2018-11-05 20:49:47 +0100

    updated run galois scripts

--- run_bfs.sh
+++ run_bfs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
 graph=$1
-PREFIX=numactl --membind=0 --physcpubind=0
+PREFIX="numactl --membind=0 --physcpubind=0"
 
-$PREFIX ./build/lonestar/bfs/bfs -t 1 $graph
+$PREFIX ./build/lonestar/bfs/bfs -t 1 $graph -noverify
--- run_pr.sh
+++ run_pr.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
 graph=$1
-PREFIX=numactl --membind=0 --physcpubind=0
+PREFIX="numactl --membind=0 --physcpubind=0"
 
-$PREFIX ./build/lonestar/pagerank/pagerank-push -t 1 $graph -tolerance=0.001
+$PREFIX ./build/lonestar/pagerank/pagerank-pull --t 1 $graph -tolerance=0.001 -noverify -algo=Topo
