e942416b6b9625fd2722c11f786a93fa8b6aa827
Dmitrii Ustiugov <dmitrii.ustiugov@epfl.ch>
2018-11-20 18:07:35 +0000

    fixed bfs startnode

--- run_bfs.sh
+++ run_bfs.sh
@@ -3,4 +3,4 @@
 graph=$1
 PREFIX="numactl --membind=0 --physcpubind=0"
 
-$PREFIX ./build/lonestar/bfs/bfs -t 1 $graph -noverify
+$PREFIX ./build/lonestar/bfs/bfs -t 1 -startNode=6157 $graph -noverify
