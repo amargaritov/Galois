9f504c56753e7e92701e85b8819b195ed7a962ad
Dmitrii Ustiugov <dmitrii.ustiugov@epfl.ch>
2018-11-23 13:11:03 +0000

    typo in pr run

--- run_pr.sh
+++ run_pr.sh
@@ -3,4 +3,4 @@
 graph=$1
 PREFIX="numactl --membind=0 --physcpubind=0"
 
-$PREFIX ./build/lonestar/pagerank/pagerank-pull --t 1 $graph -tolerance=0.001 -noverify -algo=Topo
+$PREFIX ./build/lonestar/pagerank/pagerank-pull -t 1 $graph -tolerance=0.001 -noverify -algo=Topo
